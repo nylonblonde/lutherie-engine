5ae5bb7963c768f3542c7e5e395eed4ffebe47de
lyon@nylonblonde.com <lyon@nylonblonde.com>
2019-02-11 23:15:57 -0700

    initial commit

---
+++ ECS.cpp
@@ -0,0 +1,254 @@
+#include "ECS.hpp"
+#include <iostream>
+
+System::System(World& w) : world(w) {}
+
+void System::notifyComponentChange() {
+    for(auto view : views){
+        view->updateComponents();
+    }
+}
+
+void System::Update(){
+    //do updates before OnUpdate in case any entities or components have been added or removed
+    BeforeUpdate();
+    //virtual call for the derived class, which can be user-defined
+    OnUpdate();
+    //so we can run update stuff without having to have the user define it, this might be useful for something like a command buffer, idk
+    AfterUpdate();
+}
+
+void System::OnUpdate(){}
+
+void System::BeforeUpdate() {
+    //gonna add some logic to actually ensure that only relevant componentGroups are acted on later
+    notifyComponentChange();
+}
+
+void System::AfterUpdate() {}
+
+void System::OnActive(){}
+
+std::unordered_set<size_t> System::getDependencies(){
+    return dependencies;
+}
+
+Component::Component()  {}
+
+Component::~Component() {}
+
+Entity::Entity() {}
+
+Entity::~Entity() {}
+
+
+std::list<World> World::allWorlds = {};
+uint32_t World::_staticId = 0;
+
+World::World() {
+    
+    _entityId = 0;
+    _id = _staticId;
+    _staticId+=1;
+    entities = std::vector<Entity>();
+    components = std::unordered_multimap<Entity, Component*>();
+    std::cout << "Components initialized " << components.size() << std::endl;
+    //systems are allocated in dynamic memory and will be deleted on destroyWorld
+    activeSystems = std::vector<System*>();
+    inactiveSystems = std::vector<System*>();
+
+}
+
+World::~World() {}
+
+//call destroyWorld to clean up components and systems
+void World::destroyWorld(World *&worldPtr) {
+    auto it = allWorlds.begin();
+    while(it != allWorlds.end()){
+        if(it->id() == worldPtr->id()){
+            
+            for(auto system : it->inactiveSystems){
+                delete system;
+            }
+            
+            for(auto system : it->activeSystems){
+                delete system;
+            }
+            
+            for(auto cIt = it->components.begin(); cIt != it->components.end(); cIt++){
+                delete cIt->second;
+            }
+            
+            //set the pointer used to make the call to null
+            worldPtr = nullptr;
+            it = allWorlds.erase(it);
+            break;
+        }else{
+            it++;
+        }
+    }
+    
+}
+
+
+
+Entity World::createEntity(){
+    Entity entity = Entity();
+    entity._id = _entityId;
+    entities.push_back(entity);
+    _entityId+=1;
+    return entities.back();
+}
+
+
+
+void World::setComponent(Entity entity, Component* component) {
+
+    auto its = components.equal_range(entity);
+    if(its.first != its.second){
+        for(auto it = its.first; it != its.second; ++it){
+            if(it->second->getType() == component->getType()){
+                delete it->second;
+                (*it).second = component;
+                std::cout << "Replaced component " << component->getType() << " for entity " << entity.id() << " to world " << this->id() <<  std::endl;
+                return;
+            }
+        }
+    }
+    
+    components.emplace(entity, component);
+    std::cout<< "Inserted component " << component->getType() << " for entity " << entity.id() << " in world " << this->id() << std::endl;
+    queryInactiveSystems();
+    
+}
+
+void World::updateActive(std::list<World>worlds){
+    for(World world : worlds){
+        for(auto system : world.activeSystems){
+            system->Update();
+        }
+    }
+}
+
+void World::queryInactiveSystems(){
+
+    auto it = inactiveSystems.begin();
+    while(it != inactiveSystems.end()){
+
+        std::unordered_set<size_t> dependencies = (*it)->getDependencies();
+        
+        if(dependencies.size() == 0){
+            it++;
+            continue;
+        }
+
+        std::unordered_set<size_t> tempDependencies = dependencies;
+        Entity currEntity = components.begin()->first;
+
+        bool active = false;
+        
+        auto cIt = components.begin();
+        while(cIt != components.end()){
+
+            //check to see if the component meets a dependency, erase dependency because we already know it is met, and won't need to check the next component for it
+            auto dIt = tempDependencies.find(cIt->second->getType());
+            if(dIt != tempDependencies.end()){
+                tempDependencies.erase(dIt);
+            }
+            
+            if(tempDependencies.size() == 0){
+                active = true;
+                break;
+            }
+            
+            cIt++;
+            
+            if(cIt != components.end() && currEntity != cIt->first){
+                
+                tempDependencies = dependencies;
+                currEntity = cIt->first;
+            }
+        
+        }
+
+        if(active){
+            
+            std::cout << "Adding system of type " << (*it)->getType() << " to active systems in world " << (*it)->world.id() << std::endl;
+            
+            (*it)->OnActive();
+            //remove from inactiveSystems, add to activeSystems
+            activeSystems.push_back(std::move(*it));
+            it = inactiveSystems.erase(it);
+        }else{
+            it++;
+        }
+    }
+    
+}
+
+void World::queryActiveSystems(){
+    
+    if(components.size() == 0){
+        return;
+    }
+    
+    auto it = activeSystems.begin();
+    while(it != activeSystems.end()){
+        
+        std::unordered_set<size_t> dependencies = (*it)->getDependencies();
+        
+        std::unordered_set<size_t> tempDependencies = dependencies;
+        
+        bool active = true;
+        
+        Entity currEntity = components.begin()->first;
+        
+        auto cIt = components.begin();
+        while(cIt != components.end()){
+
+            auto dIt = tempDependencies.find(cIt->second->getType());
+            if(dIt != tempDependencies.end()){
+                tempDependencies.erase(dIt);
+            }
+            
+            if(tempDependencies.size() > 0){
+                active = false;
+                break;
+            }
+            
+            cIt++;
+            
+            if(cIt != components.end() && currEntity != cIt->first){
+                
+                tempDependencies = dependencies;
+                currEntity = cIt->first;
+            }
+            
+        }
+        
+        if(!active){
+            std::cout << "Adding system of type " << (*it)->getType() << " to inactive systems in world " << (*it)->world.id() << std::endl;
+            
+            //remove from activeSystems, add to inactiveSystems
+            inactiveSystems.push_back(std::move(*it));
+            it = activeSystems.erase(it);
+        }else{
+            it++;
+        }
+    }
+}
+
+bool World::removeEntity(Entity entity){
+    auto its = components.equal_range(entity);
+    if(its.first != its.second){
+        auto it = its.first;
+        while(it != its.second){
+            delete it->second;
+            it = components.erase(it);
+        }
+        std::cout << "Removed entity " << entity.id() << " from world " << this->id() << std::endl;
+        queryActiveSystems();
+        return true;
+    }
+    return false;
+}
--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# lutherie-engine
+# Lutherie Engine
+
+Lutherie Engine is a game development engine powered by an Entity Component System pattern written in C++. Currently, the engine only features the ECS architecture, to which features like rendering will be added to as development continues.
+
+## Getting Started
+
+**Components** are structs that carry data which will be used by our `System` classes to perform logic. 
+
+```
+struct MyComponent : Component {
+    int Value;
+}
+```
+
+**Systems** are user-defined classes that are derived from the base `System` class and they are where the logic takes place. When constructed, they require a reference to the World they are created in. Later, world creation handles the initialization of the Systems, so don't worry too much about that, just know that the constructor will require that as a parameter. Systems use `ComponentGroup` definitions to determine on which entities the system will act.
+
+```
+class MySystem : System {
+public:
+    ComponentGroup<MyComponent> group = ComponentGroup<MyComponent>(*this);
+
+    MySystem(World& w) : System (w) {
+        std::cout << "Hello, World!";
+    }
+    
+    virtual void OnUpdate(){
+        //Update loop game object would go here
+    }
+}
+
+```
+
+**Worlds** host our `System` classes and their accompanying components. `World` creation is handled by a static function which takes the types that we want to register. System classes do not need to be initialized manually as they are handled in World creation.
+
+```
+World::createWorld<MySystem>();
+```
+
+On cleanup, the public method `destroyWorld()` should be called on all created worlds, as registered `System`s and attached `Component`s are stored in dynamic memory and will be deleted by this function.
+
+##Attaching Components to Entities
+
+`Component`s require an `Entity` to be "attached" to. Entities are essentially just a glorified index that Components are organized by. Entities should be created by `World`'s public `createEntity()` method. This is so indexing can be handled properly.
+
+```
+// createWorld returns a pointer to a World stored in the static list World::allWorlds
+World* world = World::createWorld<MySystem>();
+Entity entity = world->createEntity();
+```
+
+Components are allocated with `new` and are passed to the `setComponent()` method.
+
+```
+MyComponent* myComponent = new MyComponent();
+myComponent->Value = 1;
+world->setComponent(entity, myComponent);
+```
+
+Components can be removed from an Entity to stop that Entity from having any System that acts on that Component from acting on it anymore.
+
+```
+world->removeEntity<MyComponent>(entity);
+```
+
+## Making Use of Components in a System
+
+To actually use the components, we can iterate through all entities that have our dependent components attached.
+
+```
+class MySystem : System {
+public:
+    ComponentGroup<MyComponent> group = ComponentGroup<MyComponent>(*this);
+
+    MySystem(World& w) : System (w) {
+        std::cout << "Hello, World!";
+    }
+
+    virtual void OnUpdate(){
+        for(size_t i = 0; i < group.size(); ++i){
+            std::cout << group.getComponent<MyComponent>(i) << " at " << group.getEntity(i) << std::endl;
+        }
+    }
+}
+```
+
+And finally, to run an update tick, you just need to call the static `World::updateActive(World::allWorlds)`. Notice that this takes a list of worlds as a parameter, so it can be executes on all current worlds or even just a user-defined selection of them.
