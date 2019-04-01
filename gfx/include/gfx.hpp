394859b559c5de05fe733d6e8a2b76704cf70ef5
nylon_blonde <lyon@nylonblonde.com>
2019-03-31 20:21:07 -0600

    loading lutherie projects work with vulkan on linux

--- gfx/include/gfx.hpp
+++ gfx/include/gfx.hpp
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 #ifdef NDEBUG
     const bool enableValidationLayers = false;
--- src/lutherie.cpp
+++ src/lutherie.cpp
@@ -4,9 +4,19 @@
 
 Lutherie::Lutherie(const char* dir, const char* sDir, const char* rDir, const char* lDir) : projectDir(dir), scriptsDir(sDir), resourcesDir(rDir), libDir(lDir), ecs(new ECSLua(luaL_newstate(), dir)) {
 
-    setenv("VK_LAYER_PATH", std::string(std::string(dir)+std::string("etc/vulkan/explicit_layer.d")).c_str(), 1);
-    setenv("VK_ICD_FILENAMES", std::string(std::string(dir)+std::string("etc/vulkan/icd.d/MoltenVK_icd.json")).c_str(), 1);
-    
+		setenv("LD_LIBRARY_PATH", std::string(std::string(dir) + std::string("lib")).c_str(), 0);
+	#if defined (__linux__)
+
+		std::string layerPath = std::string(std::string(dir)+std::string("etc/explicit_layer.d"));
+    #elif defined(LUTHERIE_MAC)
+
+		std::string layerPath = std::string(std::string(dir)+std::string("etc/explicit_layer.d"));
+		std::string icdPath =  std::string(std::string(dir)+std::string("etc/icd.d/MoltenVK_icd.json"));
+		setenv("VK_ICD_FILENAMES", icdPath.c_str(), 1);
+
+	#endif
+		setenv("VK_LAYER_PATH", layerPath.c_str(), 1);
+
 	char * libScripts = new char[strlen(libDir) + 12];
 	strcpy(libScripts, libDir);
 	strcat(libScripts, "lua/scripts/");
@@ -17,7 +27,6 @@ Lutherie::Lutherie(const char* dir, const char* sDir, const char* rDir, const ch
     fs::doOnFilesInDir(libScripts, f);    
     fs::doOnFilesInDir(scriptsDir, f);
     
-
 #else
 	if (std::filesystem::exists(libScripts)) {
 		for (std::filesystem::directory_entry it : std::filesystem::recursive_directory_iterator(libScripts)) {
